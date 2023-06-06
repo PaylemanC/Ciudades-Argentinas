b2409c2ab678209ca647c4fa102036677d4fa6d7
Carina Payleman <rociocarina2@gmail.com>
2023-06-06 17:56:25 -0300

    Crea primer template html de citiesapp

--- citiesapp/views.py
+++ citiesapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from .models import City
+from .models import *
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hola mundo!')
+    cities = City.objects.all()
+    # City.objects.get(id=2) 
+    # City.objects.filter(name='Córdoba Capital')
+
+    # return HttpResponse('Hola mundo!')
+    return render(request, 'cities.html')
--- travelwithme/settings.py
+++ travelwithme/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'travelwithme.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['citiesapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
