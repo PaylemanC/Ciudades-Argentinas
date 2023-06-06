0e586a64ec4af02522c3240f905626724a1db77b
Carina Payleman <rociocarina2@gmail.com>
2023-06-06 17:35:57 -0300

    Crea primer modelo propio en el panel admin, prueba insertando 1 registro

--- citiesapp/admin.py
+++ citiesapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+# from .models import City
+from .models import *
 
 # Register your models here.
+class CityAdmin(admin.ModelAdmin):
+    list_display = list([field.name for field in City._meta.get_fields()]) # Llama a cada campo y lo transforma.
+
+admin.site.register(City, CityAdmin)
--- citiesapp/models.py
+++ citiesapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to="citiesapp/photos") # INSTALAR PILLOW: Librería que maneja imágenes en Django.
+    population = models.IntegerField() 
+    country = models.CharField(max_length=60)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name;
+    
+    class Meta:
+        verbose_name_plural = "Cities"
